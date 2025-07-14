027ae5c8a0b618f628e58be24fa7ce1a37c32b04
Pranjal Deka <107251617+Pranjal8983@users.noreply.github.com>
2025-07-14 12:47:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,10 +16,12 @@ module "vpc" {
 }
 
 
+
 module "security_group" {
-  source = "./modules/security-group"
-  vpc_id = module.vpc.vpc_id
+  source = "./modules/security-group"
+  vpc_id = module.vpc.vpc_id
 }
+
 
 module "alb" {
   source         = "./modules/alb"
