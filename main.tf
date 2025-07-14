061d3afa255ad32ab7a05ef7e316c47af95b99f1
Pranjal Deka <107251617+Pranjal8983@users.noreply.github.com>
2025-07-14 18:08:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,10 +17,17 @@ resource "aws_subnet" "public" {
   map_public_ip_on_launch = true
  
 }
+resource "aws_subnet" "public_a" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+ 
+}
 
 resource "aws_subnet" "private" {
   vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.2.0/24"
+  cidr_block        = "10.0.101.0/24"
   availability_zone = "us-east-1b"
  
 }
