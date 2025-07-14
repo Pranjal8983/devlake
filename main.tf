a65d12d7a8c26ab772a47f4c74cf5da64a4a78eb
Pranjal Deka <107251617+Pranjal8983@users.noreply.github.com>
2025-07-14 18:42:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -25,7 +25,7 @@ resource "aws_subnet" "public_a" {
  
 }
 
-resource "aws_subnet" "private" {
+resource "aws_subnet" "private_subnets" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.3.0/24"
   availability_zone = "us-east-1b"
