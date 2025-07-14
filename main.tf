48d214a61101d2916bb42be83e606101909899e5
Pranjal Deka <107251617+Pranjal8983@users.noreply.github.com>
2025-07-14 15:05:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 module "ec2" {
   source         = "./modules/ec2"
   ami_id         = var.ami_id
-  vpc_id         = module.vpc.vpc_id
+  vpc_id         = aws_vpc.main.id
   sg_id          = aws_security_group.security_group.id
   public_subnets = module.vpc.public_subnet_ids
 }
