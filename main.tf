ea672d00f31a41b844804a0c2c3eb032251f140b
Pranjal Deka <107251617+Pranjal8983@users.noreply.github.com>
2025-07-14 16:10:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,31 +1,159 @@
-module "ec2" {
-  source         = "./modules/ec2"
-  ami_id         = var.ami_id
-  vpc_id         = module.vpc.vpc_id
-  sg_id          = module.security_group.sg_id
-  public_subnets = module.vpc.public_subnet_ids
+resource "aws_vpc" "main" {
+  cidr_block           = 10.0.0.0/16
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "${var.vpc_name}"
+  }
 }
 
-module "vpc" {
-  source          = "./modules/vpc"
-  vpc_name        = var.vpc_name
-  vpc_cidr        = var.vpc_cidr
-  public_subnets  = var.public_subnets
-  private_subnets = var.private_subnets
-  azs             = var.azs
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "${var.vpc_name}-igw"
+  }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "${var.vpc_name}-public-${count.index}"
+  }
+}
+
+resource "aws_subnet" "private" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
+  tags = {
+    Name = "${var.vpc_name}-private-${count.index}"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  tags = {
+    Name = "${var.vpc_name}-public-rt"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  count          = length(aws_subnet.public)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+resource "aws_security_group" "security_group" {
+  name        = "uc1-sg"
+  description = "Allow HTTP traffic only from ALB"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+   ingress {
+    from_port   = 4000
+    to_port     = 4000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "alb_security_group" {
+  name        = "uc1-alb-sg"
+  description = "Allow HTTP traffic only from ALB"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws_instance" "ec2"{
+    ami = var.ami_id
+    instance_type = "t2.medium"
+    subnet_id = var.public_subnets[0]
+    vpc_security_group_ids = [var.sg_id]
+    associate_public_ip_address = true
+    user_data = file("${path.module}/userdata-0.sh")
+    tags = {
+      Name = "Instance-0"
+    }
+}
+resource "aws_lb" "this" {
+  name               = var.name
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [var.alb_sg_id]
+  subnets            = var.public_subnets
+
+  tags = {
+    Name = var.name
+  }
+}
+
+# Target Groups
+resource "aws_lb_target_group" "default" {
+  name     = "${var.name}-tg-default"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
 }
 
 
-module "security_group" {
-  source = "./modules/security-group"
-  vpc_id = module.vpc.vpc_id
+# Listener
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.this.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.default.arn
+  }
 }
 
-module "alb" {
-  source         = "./modules/alb"
-  name           = "myapp-alb"
-  vpc_id         = module.vpc.vpc_id
-  public_subnets = module.vpc.public_subnet_ids
-  alb_sg_id      = module.security_group.alb_sg_id
-  instance_ids   = module.ec2.instance_ids
+
+resource "aws_lb_target_group_attachment" "default" {
+  target_group_arn = aws_lb_target_group.default.arn
+  target_id        = var.instance_ids
+  port             = 4000
 }
