5bda7940150e51008a876179f63761e3e075bc0c
Pranjal Deka <107251617+Pranjal8983@users.noreply.github.com>
2025-07-14 18:09:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -43,7 +43,7 @@ resource "aws_route_table" "public" {
 
 resource "aws_route_table_association" "public" {
   count          = length(aws_subnet.public)
-  subnet_id      = aws_subnet.public[count.index].id
+  subnet_id      = aws_subnet.public.id
   route_table_id = aws_route_table.public.id
 }
 resource "aws_security_group" "security_group" {
