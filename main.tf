f1382f4b5b9e5f58fa97caf3d6dea57ee06e2614
Pranjal Deka <107251617+Pranjal8983@users.noreply.github.com>
2025-07-14 14:57:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,11 +17,59 @@ module "vpc" {
 
 
 
-module "security_group" {
-  source = "./modules/security-group"
-  vpc_id = module.vpc.vpc_id
+resource "aws_security_group" "security_group" {
+  name        = "uc1-sg"
+  description = "Allow HTTP traffic only from ALB"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+   ingress {
+    from_port   = 4000
+    to_port     = 4000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
+resource "aws_security_group" "alb_security_group" {
+  name        = "uc1-alb-sg"
+  description = "Allow HTTP traffic only from ALB"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 
 module "alb" {
   source         = "./modules/alb"
